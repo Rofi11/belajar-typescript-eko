a3103804197d1d6d1a11531c3911782c7a85e1a1
Muhammad Rofi <98885565+Rofi11@users.noreply.github.com>
2024-09-11 17:53:54 +0700

    Ternary Operator (31) : Membuat Ternary Operator mirip seperti di javascript

---
+++ dist/test/31-ternary-operator.test.js
@@ -0,0 +1,9 @@
+"use strict";
+describe("Ternary Operator", () => {
+    it("Should support Ternary Operator in Typescript", () => {
+        const value = 80;
+        //Ternary operator
+        const say = value >= 75 ? "Congratulation" : "Try Again";
+        console.info(say);
+    });
+});
---
+++ test/31-ternary-operator.test.ts
@@ -0,0 +1,8 @@
+describe("Ternary Operator", () => {
+  it("Should support Ternary Operator in Typescript", () => {
+    const value: number = 80;
+    //Ternary operator
+    const say = value >= 75 ? "Congratulation" : "Try Again";
+    console.info(say);
+  });
+});
