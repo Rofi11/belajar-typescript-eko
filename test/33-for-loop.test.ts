2d350ab87ad17fc88c2f94aad35de26bab2e9cea
Muhammad Rofi <98885565+Rofi11@users.noreply.github.com>
2024-09-13 17:57:04 +0700

    for loop (33) : membuat loop , sama dengan di javascript

---
+++ test/33-for-loop.test.ts
@@ -0,0 +1,20 @@
+describe("For loop", () => {
+  it("Should support For loop in Typescript", () => {
+    const names: string[] = ["Eko", "Kurniawan", "Khannedy"];
+
+    // for loop biasa
+    for (let i = 0; i < names.length; i++) {
+      console.info(names[i]);
+    }
+
+    // for of
+    for (const name of names) {
+      console.info(name);
+    }
+
+    // for in
+    for (const index in names) {
+      console.info(names[index]);
+    }
+  });
+});
