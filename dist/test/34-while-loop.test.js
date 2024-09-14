d2f7c585922c344eb864fe1efdb8b9d70864382e
Muhammad Rofi <98885565+Rofi11@users.noreply.github.com>
2024-09-14 17:18:25 +0700

    While Loop (34) : membuat while loop, ini sama seperti di javascript

---
+++ dist/test/34-while-loop.test.js
@@ -0,0 +1,10 @@
+"use strict";
+describe("While loop", () => {
+    it("Should support While Loop in Typescript", () => {
+        let counter = 0;
+        while (counter < 10) {
+            console.info(counter);
+            counter++;
+        }
+    });
+});
---
+++ test/34-while-loop.test.ts
@@ -0,0 +1,10 @@
+describe("While loop", () => {
+  it("Should support While Loop in Typescript", () => {
+    let counter: number = 0;
+
+    while (counter < 10) {
+      console.info(counter);
+      counter++;
+    }
+  });
+});
