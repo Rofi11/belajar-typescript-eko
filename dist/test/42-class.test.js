429489b8ccd96fce40013bf723c207c54d78a5e6
Muhammad Rofi <98885565+Rofi11@users.noreply.github.com>
2024-09-19 16:18:07 +0700

    Class (42) : membuat class dan membuat object dari class

---
+++ dist/test/42-class.test.js
@@ -0,0 +1,14 @@
+"use strict";
+describe("Class", () => {
+    it("Should support Class in Typescript", () => {
+        // membuat class
+        class Customer {
+        }
+        class Order {
+        }
+        // membuat object dari class dengan deklare
+        const customer = new Customer();
+        // membuat object dari class secara explisit
+        const order = new Order();
+    });
+});
---
+++ test/42-class.test.ts
@@ -0,0 +1,13 @@
+describe("Class", () => {
+  it("Should support Class in Typescript", () => {
+    // membuat class
+    class Customer {}
+
+    class Order {}
+
+    // membuat object dari class dengan deklare
+    const customer: Customer = new Customer();
+    // membuat object dari class secara explisit
+    const order = new Order();
+  });
+});
